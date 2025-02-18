d5442d985a033fb25009898f5f758e7a8813d382
gabrielgonta <146724165+gabrielgonta@users.noreply.github.com>
2025-02-19 00:22:05 +0100

    [+] Add Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer, Navbar } from "./components";
 import {
@@ -18,20 +18,18 @@ const App = () => {
   return (
     <>
       <div className="font-Nunito overflow-hidden max-w-[1440px] mx-auto">
-        <BrowserRouter basename="/Gabibook">
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Flights />} />
-            <Route path="/hotels" element={<Hotels />} />
-            <Route path="/packages" element={<Packages />} />
-            <Route path="/explore" element={<FlightExplore />} />
-            <Route path="/passenger-info" element={<PassengerInfo />} />
-            <Route path="/seat-selection" element={<SeatSelect />} />
-            <Route path="/payment" element={<Payment />} />
-            <Route path="/confirm" element={<Confirm />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Flights />} />
+          <Route path="/hotels" element={<Hotels />} />
+          <Route path="/packages" element={<Packages />} />
+          <Route path="/explore" element={<FlightExplore />} />
+          <Route path="/passenger-info" element={<PassengerInfo />} />
+          <Route path="/seat-selection" element={<SeatSelect />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/confirm" element={<Confirm />} />
+        </Routes>
+        <Footer />
         <ToastContainer
           position="bottom-center"
           autoClose={5000}
